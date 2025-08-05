19c466aed8498a1eda985b56a53892d6aae15669
jar <dev.bazylev@gmail.com>
2025-08-05 20:46:33 +0300

    fix: selector

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,11 +1,12 @@
 import Card from '../card/card';
 import { useAppSelector } from '../../hooks';
+import { getCards } from '../../store/site-data/selectors';
 // eslint-disable-next-line no-console
 /* eslint-disable */
 // @ts-ignore
 
 function CardList(): JSX.Element {
-  const cards = useAppSelector(selectCards);
+  const cards = useAppSelector(getCards);
 
   return (
     <ul className="info__list">
