dcb718c51cb8d072a3c02abff5cccbd57e26a98c
jar <dev.bazylev@gmail.com>
2025-08-06 23:42:50 +0300

    feat: add mock armor images

--- src/components/checkbox-list/checkbox-list.tsx
+++ src/components/checkbox-list/checkbox-list.tsx
@@ -4,10 +4,10 @@ function CheckboxList(): JSX.Element {
       <button className="btn info__toggler" type="button" aria-label="Toggle checkbox menu.">Checkbox filters</button>
       <div className="info__choice">
         <label className="info__label btn">
-          <input className="visually-hidden info__check" type="checkbox" name="Magic items" id="mi" tabIndex={-1} defaultChecked /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Magic&nbsp;items</span>
+          <input className="visually-hidden info__check" type="checkbox" name="Magic items" id="mi" tabIndex={-1} /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Magic&nbsp;items</span>
         </label>
         <label className="info__label btn">
-          <input className="visually-hidden info__check" type="checkbox" name="Weapons" id="weapons" tabIndex={-1} /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Weapons</span>
+          <input className="visually-hidden info__check" type="checkbox" name="Weapons" id="weapons" tabIndex={-1} defaultChecked /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Weapons</span>
         </label>
         <label className="info__label btn">
           <input className="visually-hidden info__check" type="checkbox" name="Elexirs" id="elexirs" tabIndex={-1} /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Elexirs</span>
@@ -16,7 +16,7 @@ function CheckboxList(): JSX.Element {
           <input className="visually-hidden info__check" type="checkbox" name="Artefacts" id="artefacts" tabIndex={-1} /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Artefacts</span>
         </label>
         <label className="info__label btn">
-          <input className="visually-hidden info__check" type="checkbox" name="Armors" id="armors" tabIndex={-1} /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Armors</span>
+          <input className="visually-hidden info__check" type="checkbox" name="Armors" id="armors" tabIndex={-1} defaultChecked /><span className="info__backup" tabIndex={1} /><span className="info__checktext">Armors</span>
         </label>
       </div>
     </div>
