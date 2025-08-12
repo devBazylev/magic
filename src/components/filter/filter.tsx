a6ea8a5c407eb05ed7d0e46d7e686310ac4fb836
jar <dev.bazylev@gmail.com>
2025-08-12 18:55:45 +0300

    refactor

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -5,7 +5,7 @@ function Filter(): JSX.Element {
     <div className="info__wrap">
       <div className="info__num">0 items</div>
       <div className="info__select">
-        <button className="btn info__board" type="button">${filters[0]}&nbsp;first</button>
+        <button className="btn info__board" type="button">{filters[0]}&nbsp;first</button>
         <div className="info__drop">
           {/* {filters.map((filter) => {
             <div className=`info__option info__option--{filter.toLowerCase()}`>{filter}&nbsp;first</div>
