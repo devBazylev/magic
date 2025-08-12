4174dd7e6c6279f2017a59c967f04125149f1764
jar <dev.bazylev@gmail.com>
2025-08-12 19:25:11 +0300

    fix: double rerender

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import type { State, AppDispatch } from './types';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { setMedia } from './store/action';
@@ -6,15 +7,20 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
 
 export const useMedia = () => {
-  const mob: MediaQueryList = window.matchMedia('(max-width: 1023px)');
   const dispatch = useAppDispatch();
-  dispatch(setMedia(mob.matches));
 
-  const onMOb = () => {
+  useEffect(() => {
+    const mob: MediaQueryList = window.matchMedia('(max-width: 1023px)');
     dispatch(setMedia(mob.matches));
-    // eslint-disable-next-line no-console
-    console.log(123);
-  };
 
-  mob.addEventListener('change', onMOb);
+    const onMOb = () => {
+      dispatch(setMedia(mob.matches));
+    };
+
+    mob.addEventListener('change', onMOb);
+
+    return () => {
+      mob.removeEventListener('change', onMOb);
+    };
+  }, [dispatch]);
 };
