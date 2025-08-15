4d7e5cbf4e5f363b5bbabe7e23d7f728c2fc7b63
jar <dev.bazylev@gmail.com>
2025-08-15 16:48:16 +0300

    refactor

--- src/store/site-data/selectors.ts
+++ src/store/site-data/selectors.ts
@@ -1,16 +1,5 @@
-// import { createSelector } from '@reduxjs/toolkit';
 import type { State, CardProps } from '../../types';
 import { StoreSlice } from '../../const';
-// import { getSorting } from '../site-process/selectors';
 
 export const getIsCardsLoading = ({ [StoreSlice.SiteData]: SITE_DATA }: State): boolean => SITE_DATA.isCardsLoading;
-
 export const getCards = ({ [StoreSlice.SiteData]: SITE_DATA }: State): CardProps[] => SITE_DATA.cards;
-
-// export const selectCards = createSelector(
-//   [getCards, getSorting],
-//   (cards, sorting) => {
-//     const comparator = Comprator[sorting as keyof typeof Comprator];
-//     return cards.sort(comparator);
-//   }
-// );
