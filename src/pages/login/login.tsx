4c2ace02978ddc2e5cede305cc74b0418ba891c1
jar <dev.bazylev@gmail.com>
2025-08-19 16:33:46 +0300

    feat: add signup btn and styles && add basic logic to login page without server requests

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,25 +1,47 @@
 import Header from '../../components/header/header';
 import Modal from '../../components/modal/modal';
 import Overlay from '../../components/overlay/overlay';
-import { HelmetProvider } from 'react-helmet-async';
-import type { FormEvent } from 'react';
+import Back from '../../components/back/back';
 import type { UserAuth } from '../../types';
+import { HelmetProvider } from 'react-helmet-async';
+import { BackPath } from '../../const';
+import { FormEvent, useRef, useState } from 'react';
 import { useAppDispatch } from '../../hooks';
 import { loginUser } from '../../store/action';
-import { BackPath } from '../../const';
-import Back from '../../components/back/back';
-import { useRef } from 'react';
 
 function Login(): JSX.Element {
-  const dispatch = useAppDispatch();
+  const [method, setMethod] = useState('get');
   const headerRef = useRef<HTMLHeadingElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+  const dispatch = useAppDispatch();
+
+  const handleButtonClick = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    if (formRef.current) {
+      formRef.current.classList.add('login__form--check');
+    }
+    if (evt.currentTarget.classList.contains('login__submit--signin')) {
+      setMethod('get');
+    } else if (evt.currentTarget.classList.contains('login__submit--signup')) {
+      setMethod('post');
+    }
+  };
 
   const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData) as UserAuth;
-    dispatch(loginUser(data));
+    const form = formRef.current;
+    if (form) {
+      evt.preventDefault();
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData) as UserAuth;
+      if (method === 'get') {
+        dispatch(loginUser(data));
+      } else if (method === 'post') {
+        // dispatch(registerUser(data));
+      }
+
+      // eslint-disable-next-line no-console
+      console.log('Form submitted!');
+      formRef.current.classList.remove('login__form--check');
+    }
   };
 
   return (
@@ -31,12 +53,13 @@ function Login(): JSX.Element {
       <main>
         <section className="login">
           <h1 className="login__title">Login</h1>
-          <form className="login__form" action="#" method="post" onSubmit={handleFormSubmit}>
-            <input className="login__input" type="text" name="email" id="email" minLength={5} maxLength={40} title="Lenght 5 - 40 symbols" autoComplete="email" required />
+          <form className="login__form" action="#" method="post" onSubmit={handleFormSubmit} ref={formRef}>
+            <input className="login__input" type="email" name="email" id="email" minLength={5} maxLength={40} title="Lenght 5 - 40 symbols" autoComplete="email" required />
             <label className="login__label" key="email" htmlFor="email">Email</label>
             <input className="login__input" type="password" name="password" id="password" minLength={3} maxLength={40} title="Lenght 3 - 40 symbols" autoComplete="password" required />
             <label className="login__label" key="password" htmlFor="password">Password</label>
-            <button className="login__submit btn" type="submit">Sign in</button>
+            <button className="login__submit login__submit--signin btn" type="submit" onClick={handleButtonClick}>Sign in</button>
+            <button className="login__submit login__submit--signup btn" type="submit" onClick={handleButtonClick}>Sign up</button>
           </form>
         </section>
         <Modal headerRef={headerRef} />
